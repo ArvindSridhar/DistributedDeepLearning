9a386fd5081f9f6401f2feff89af91e6abcca142
Arvind Sridhar <arvindsridhar@berkeley.edu>
2018-08-09 00:33:49 -0700

    Cleaned up code, clear workflow

---
+++ commented_code.py
@@ -0,0 +1,68 @@
+		#--------------------------------------------------------------------------------
+
+		# for segment in self.segment_models:
+		# 	score = self.segment_models[segment].evaluate(self.x_test, self.y_test, verbose=0)
+		# 	print('[Segment ' + str(segment) + '] Test loss: ' + str(score[0]))
+		# 	print('[Segment ' + str(segment) + '] Test accuracy: ' + str(score[1]))
+
+		#--------------------------------------------------------------------------------
+
+		# aggregate_model = Sequential()
+		# aggregate_model.add(Dense(512, activation='relu', input_shape=(784,)))
+		# aggregate_model.add(Dropout(0.2))
+		# aggregate_model.add(Dense(512, activation='relu'))
+		# aggregate_model.add(Dropout(0.2))
+		# aggregate_model.add(Dense(self.num_classes, activation='softmax'))
+
+		# avg_weights = sum([np.array(self.segment_models[segment].get_weights()) for segment in self.segment_models])/self.num_segments
+		# aggregate_model.set_weights(avg_weights)
+
+		# aggregate_model.compile(loss='categorical_crossentropy',
+  #           optimizer=Adam(),
+  #           metrics=['accuracy'])
+
+		# score = aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
+		# print(score)
+
+		# for segment in self.segment_models:
+		# 	self.segment_models[segment] = clone_model(aggregate_model)
+
+		# for segment in self.segment_models:
+		# 	# model_seg = self.segment_models[segment]
+		# 	(x_train_seg, y_train_seg) = self.segment_batches[segment]				
+		# 	# self.segment_models[segment] = self.train_model_segment(model_seg, x_train_seg, y_train_seg)
+		# 	self.segment_models[segment].compile(loss='categorical_crossentropy',
+	 #            optimizer=Adam(),
+	 #            metrics=['accuracy'])
+
+		# 	history = self.segment_models[segment].fit(x_train_seg, y_train_seg,
+		#         batch_size=self.batch_size,
+		#         epochs=5,
+		#         verbose=1,
+		#         validation_data=(self.x_test, self.y_test))
+
+		# avg_weights = sum([np.array(self.segment_models[segment].get_weights()) for segment in self.segment_models])/self.num_segments
+		# aggregate_model.set_weights(avg_weights)
+
+		# aggregate_model.compile(loss='categorical_crossentropy',
+  #           optimizer=Adam(),
+  #           metrics=['accuracy'])
+
+		# score = aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
+		# print(score)
+
+		# count = 0
+		# for segment in self.segment_models:
+		# 	model_seg = self.segment_models[segment]
+		# 	seg_weights = model_seg.get_weights()
+		# 	print(seg_weights.shape)
+		# 	if not weights:
+		# 		weights = seg_weights
+		# 		count += 1
+		# 	else:
+		# 		weights = (weights*count + seg_weights)
+		# 		count += 1
+		# 		weights /= count
+
+		# def train_model_segment(self, model, x_train, y_train):
+		# return model
--- distributed_model_training.py
+++ distributed_model_training.py
@@ -6,19 +6,21 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop, Adam
+from keras.optimizers import RMSprop, Adam, SGD
 from keras.models import clone_model
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
+import math
 
 class distributed_model_training:
 
 	def __init__(self):
 		self.num_classes = 10
-		self.total_num_epochs = 1 #to change
+		self.total_num_epochs = 4 #Can tune
 		self.batch_size = 100
-		self.num_segments = 4
+		self.num_segments = 5 #Can tune
+		self.num_iters_on_segment = 4 #Can tune
 		self.get_data()
 		self.distribute_data()
 		self.define_models()
@@ -39,16 +41,17 @@ class distributed_model_training:
 		self.x_test = x_test.astype('float32')
 		self.x_train /= 255
 		self.x_test /= 255
-		#One-hot encode the y vectors
+		# One-hot encode the y vectors
 		self.y_train = keras.utils.to_categorical(self.y_train, self.num_classes)
 		self.y_test = keras.utils.to_categorical(self.y_test, self.num_classes)
 
 	def distribute_data(self):
-		#only deal with x_train and y_train
-		# np.random.shuffle(self.x_train)
+		# Shuffle? np.random.shuffle(self.x_train)
 		self.segment_batches = {}
+		data_per_segment = int(math.floor(60000/self.num_segments))
 		for i in range(self.num_segments):
-			self.segment_batches["seg"+str(i)] = (self.x_train[6000*i:6000*i+6000], self.y_train[6000*i:6000*i+6000])
+			self.segment_batches["seg"+str(i)] = (self.x_train[data_per_segment*i:data_per_segment*i+data_per_segment],
+												  self.y_train[data_per_segment*i:data_per_segment*i+data_per_segment])
 
 	def define_models(self):
 		self.segment_models = {}
@@ -59,95 +62,55 @@ class distributed_model_training:
 			model.add(Dense(512, activation='relu'))
 			model.add(Dropout(0.2))
 			model.add(Dense(self.num_classes, activation='softmax'))
-
-			model.compile(loss='categorical_crossentropy',
-	            optimizer=Adam(),
-	            metrics=['accuracy'])
-
 			self.segment_models["seg"+str(i)] = model
 
 	def train_model_aggregate(self):
+		# Training and evaluation loop
 		for i in range(self.total_num_epochs):
-			for segment in self.segment_models:
-				# model_seg = self.segment_models[segment]
+			print("Grand Epoch:", i, "/", self.total_num_epochs)
+			
+			# Re-define the aggregate model (stored on the master node, and ultimately returned), also re-initialize its weights
+			self.aggregate_model = Sequential()
+			self.aggregate_model.add(Dense(512, activation='relu', input_shape=(784,)))
+			self.aggregate_model.add(Dropout(0.2))
+			self.aggregate_model.add(Dense(512, activation='relu'))
+			self.aggregate_model.add(Dropout(0.2))
+			self.aggregate_model.add(Dense(self.num_classes, activation='softmax'))
+
+			# Train individual models for 5 epochs
+			for segment in sorted(self.segment_models):
+				print('Segment:', segment)
 				(x_train_seg, y_train_seg) = self.segment_batches[segment]				
-				# self.segment_models[segment] = self.train_model_segment(model_seg, x_train_seg, y_train_seg)
+				self.segment_models[segment].compile(loss='categorical_crossentropy',
+		            optimizer=Adam(),
+		            metrics=['accuracy'])
 				history = self.segment_models[segment].fit(x_train_seg, y_train_seg,
 			        batch_size=self.batch_size,
-			        epochs=5,
+			        epochs=self.num_iters_on_segment,
 			        verbose=1,
 			        validation_data=(self.x_test, self.y_test))
 
-		#--------------------------------------------------------------------------------
+			# Average the weights of the trained models on the segments, add these weights to the aggregate model
+			avg_weights = sum([np.array(self.segment_models[segment].get_weights()) for segment in self.segment_models])/self.num_segments
+			self.aggregate_model.set_weights(avg_weights)
 
-		for segment in self.segment_models:
-			score = self.segment_models[segment].evaluate(self.x_test, self.y_test, verbose=0)
-			print('[Segment ' + str(segment) + '] Test loss: ' + str(score[0]))
-			print('[Segment ' + str(segment) + '] Test accuracy: ' + str(score[1]))
-
-		#--------------------------------------------------------------------------------
-
-		aggregate_model = Sequential()
-		aggregate_model.add(Dense(512, activation='relu', input_shape=(784,)))
-		aggregate_model.add(Dropout(0.2))
-		aggregate_model.add(Dense(512, activation='relu'))
-		aggregate_model.add(Dropout(0.2))
-		aggregate_model.add(Dense(self.num_classes, activation='softmax'))
-
-		avg_weights = sum([np.array(self.segment_models[segment].get_weights()) for segment in self.segment_models])/self.num_segments
-		aggregate_model.set_weights(avg_weights)
-
-		aggregate_model.compile(loss='categorical_crossentropy',
-            optimizer=Adam(),
-            metrics=['accuracy'])
-
-		score = aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
-		print(score)
-
-		for segment in self.segment_models:
-			self.segment_models[segment] = clone_model(aggregate_model)
-
-		for segment in self.segment_models:
-			# model_seg = self.segment_models[segment]
-			(x_train_seg, y_train_seg) = self.segment_batches[segment]				
-			# self.segment_models[segment] = self.train_model_segment(model_seg, x_train_seg, y_train_seg)
-			self.segment_models[segment].compile(loss='categorical_crossentropy',
+			# Compile aggregate model
+			self.aggregate_model.compile(loss='categorical_crossentropy',
 	            optimizer=Adam(),
 	            metrics=['accuracy'])
 
-			history = self.segment_models[segment].fit(x_train_seg, y_train_seg,
-		        batch_size=self.batch_size,
-		        epochs=5,
-		        verbose=1,
-		        validation_data=(self.x_test, self.y_test))
+			# Evaluate aggregate model on the test set
+			score = self.aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
+			print(score)
 
-		avg_weights = sum([np.array(self.segment_models[segment].get_weights()) for segment in self.segment_models])/self.num_segments
-		aggregate_model.set_weights(avg_weights)
+			# Redistribute the aggregate model to each segment for the next epoch of training
+			for segment in self.segment_models:
+				self.segment_models[segment] = clone_model(self.aggregate_model)
 
-		aggregate_model.compile(loss='categorical_crossentropy',
-            optimizer=Adam(),
-            metrics=['accuracy'])
-
-		score = aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
-		print(score)
-
-		# count = 0
-		# for segment in self.segment_models:
-		# 	model_seg = self.segment_models[segment]
-		# 	seg_weights = model_seg.get_weights()
-		# 	print(seg_weights.shape)
-		# 	if not weights:
-		# 		weights = seg_weights
-		# 		count += 1
-		# 	else:
-		# 		weights = (weights*count + seg_weights)
-		# 		count += 1
-		# 		weights /= count
-
-
-	def train_model_segment(self, model, x_train, y_train):
-		return model
-
+		# Conduct final testing of aggregate model
+		train_score = self.aggregate_model.evaluate(self.x_train, self.y_train, verbose=1)
+		test_score = self.aggregate_model.evaluate(self.x_test, self.y_test, verbose=1)
+		print("Training accuracy:", train_score[1])
+		print("Test accuracy:", test_score[1])
 
 model_instance = distributed_model_training()
-# model_instance.get_data()
