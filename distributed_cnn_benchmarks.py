57286b56723a524c6ca9794652b4083735efb519
Arvind Sridhar <arvindsridhar@berkeley.edu>
2018-12-22 23:19:24 -0800

    Trying VGG-16

--- distributed_cnn_benchmarks.py
+++ distributed_cnn_benchmarks.py
@@ -24,8 +24,8 @@ class distributed_cnn_benchmark:
 		self.num_classes = 10
 		self.batch_size = 300
 		self.num_segments = 10
-		self.num_training_iterations = 400
-		self.num_iters_on_segment = 1
+		self.num_training_iterations = 20
+		self.num_iters_on_segment = 2
 		self.cached_predictions = {}
 		self.cifar = True
 		self.tensorflow_device_test()
@@ -286,17 +286,155 @@ distrcnn = distributed_cnn_benchmark()
 sercnn = serial_cnn_benchmark()
 
 # Define the model to be used
-model = Sequential()
-model.add(Conv2D(16, kernel_size=(3, 3),
-	activation='relu',
-	input_shape=distrcnn.input_shape))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(distrcnn.num_classes, activation='softmax'))
+def create_model_architecture(n_classes, input_shape, config='A'):
+	model = Sequential()
+
+	# Determine proper input shape
+	# input_shape = (224,224,3)
+	# input_shape = _obtain_input_shape(input_shape,
+	# 							  default_size=224,
+	# 							  min_size=48,
+	# 							  data_format=K.image_data_format(),
+	# 							  require_flatten=True)
+
+	inputs = Input(shape=input_shape)
+
+	# Block 1
+	x = Conv2D(filters=64, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block1_conv1')(inputs)
+
+	if (config != 'A'):
+		x = Conv2D(filters=64, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block1_conv2')(x)
+
+	x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="block1_pool", padding='valid')(x)
+
+	# Block 2
+	x = Conv2D(filters=128, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block2_conv1')(x)
+
+	if (config != 'A'):
+		x = Conv2D(filters=128, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block2_conv2')(x)
+
+	x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="block2_pool", padding='valid')(x)
+
+	# Block 3
+	x = Conv2D(filters=256, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block3_conv1')(x)
+
+	x = Conv2D(filters=256, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block3_conv2')(x)
+
+	if (config != 'A' and config != 'B'):
+		if config == 'C':
+			ks = 1
+		else:
+			ks = 3
+			x = Conv2D(filters=256, kernel_size=ks, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block3_conv3')(x)
+
+	if (config == 'E'):
+		x = Conv2D(filters=256, kernel_size=ks, strides=(1, 1), padding='same',
+			kernel_regularizer=l2(0.0002),
+			activation='relu', name='block3_conv3')(x)
+
+	x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="block3_pool", padding='valid')(x)
+
+	# Block 4
+	x = Conv2D(filters=512, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block4_conv1')(x)
+
+	x = Conv2D(filters=512, kernel_size=3, strides=(1, 1), padding='same',
+			   kernel_regularizer=l2(0.0002),
+			   activation='relu', name='block4_conv2')(x)
+
+	if (config != 'A' and config != 'B'):
+		if config == 'C':
+			ks = 1
+		else:
+			ks = 3
+		x = Conv2D(filters=512, kernel_size=ks, strides=(1, 1), padding='same',
+				   kernel_regularizer=l2(0.0002),
+				   activation='relu', name='block4_conv3')(x)
+
+		if config == 'E':
+			x = Conv2D(filters=512, kernel_size=ks, strides=(1, 1), padding='same',
+				   kernel_regularizer=l2(0.0002),
+				   activation='relu', name='block4_conv3')(x)
+
+	x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="block4_pool", padding='valid')(x)
+
+	# Block 5
+
+	if (config != 'A' and config != 'B'):
+		if config == 'C':
+			ks = 1
+		else:
+			ks = 3
+		x = Conv2D(filters=512, kernel_size=ks, strides=(1, 1), padding='same',
+				   kernel_regularizer=l2(0.0002),
+				   activation='relu', name='block4_conv3')(x)
+
+		if config == 'E':
+			x = Conv2D(filters=512, kernel_size=ks, strides=(1, 1), padding='same',
+				   kernel_regularizer=l2(0.0002),
+				   activation='relu', name='block4_conv3')(x)
+
+	x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="block5_pool", padding='valid')(x)
+
+	# Classification block
+	x = Flatten(name='flatten')(x)
+	x = Dense(4096, activation='relu', name='fc1')(x)
+	x = Dropout(0.5, name='drop_fc1')(x)
+
+	x = Dense(4096, activation='relu', name='fc2')(x)
+	x = Dropout(0.5, name='drop_fc2')(x)
+
+	x = Dense(n_classes, activation='softmax', name="predictions")(x)
+
+	model = Model(inputs, x, name='vgg16-places365')
+
+	sgd = SGD(lr=0.01, decay=1e-6, nesterov=True)
+	model.summary()
+
+	model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+	return model
+
+model = create_model_architecture(distrcnn.num_classes, distrcnn.input_shape)
+
+
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3),
+# 	activation='relu',
+# 	input_shape=distrcnn.input_shape))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(distrcnn.num_classes, activation='softmax'))
+
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3),
+# 	activation='relu',
+# 	input_shape=distrcnn.input_shape))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(32, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(distrcnn.num_classes, activation='softmax'))
+
 # Older model:
 # model = Sequential()
 # model.add(Conv2D([32/64], kernel_size=(3, 3),
